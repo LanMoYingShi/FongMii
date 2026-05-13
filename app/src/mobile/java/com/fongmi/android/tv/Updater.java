c4fc7c71d2d0d915e1f46c31f4855bbef1db3423
IsayIsee <solin.zhan@gmail.com>
2026-05-13 10:23:56 +0800

    升级前显示当前版本

--- app/src/mobile/java/com/fongmi/android/tv/Updater.java
+++ app/src/mobile/java/com/fongmi/android/tv/Updater.java
@@ -67,6 +67,8 @@ public class Updater implements Download.Callback {
 
     private void doInBackground(Activity activity) {
         try {
+            Notify.show(ResUtil.getString(R.string.update_current_version, BuildConfig.VERSION_CODE));
+            Notify.show("URL:" + getJson());
             JSONObject object = new JSONObject(OkHttp.string(getJson()));
             String name = object.optString("name");
             String desc = object.optString("desc");
